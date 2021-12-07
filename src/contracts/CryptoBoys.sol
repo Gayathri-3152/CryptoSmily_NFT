889b405d915a19e4e0a753620d5eec5d9e4450d4
Gayathri-3152 <77276255+Gayathri-3152@users.noreply.github.com>
2021-12-07 13:50:57 +0530

    initial commit

--- src/contracts/CryptoBoys.sol
+++ src/contracts/CryptoBoys.sol
@@ -19,7 +19,6 @@ contract CryptoBoys is ERC721 {
     address payable previousOwner;
     uint256 price;
     uint256 numberOfTransfers;
-    //bool forSale;
 
     bool forBidding;
     uint256 biddingPrice;
@@ -100,78 +99,17 @@ contract CryptoBoys is ERC721 {
     return tokenExists;
   }
 
-  /*function buyToken(uint256 _tokenId) public payable {
-    // check if the function caller is not an zero account address
-    require(msg.sender != address(0));
-    // check if the token id of the token being bought exists or not
-    require(_exists(_tokenId));
-    // get the token's owner
-    address tokenOwner = ownerOf(_tokenId);
-    // token's owner should not be an zero address account
-    require(tokenOwner != address(0));
-    // the one who wants to buy the token should not be the token's owner
-    require(tokenOwner != msg.sender);
-    // get that token from all crypto boys mapping and create a memory of it defined as (struct => CryptoBoy)
-    CryptoBoy memory cryptoboy = allCryptoBoys[_tokenId];
-    // price sent in to buy should be equal to or more than the token's price
-    require(msg.value >= cryptoboy.price);
-    // token should be for sale
-    require(cryptoboy.forSale);
-    // transfer the token from owner to the caller of the function (buyer)
-    _transfer(tokenOwner, msg.sender, _tokenId);
-    // get owner of the token
-    address payable sendTo = cryptoboy.currentOwner;
-    // send token's worth of ethers to the owner
-    sendTo.transfer(msg.value);
-    // update the token's previous owner
-    cryptoboy.previousOwner = cryptoboy.currentOwner;
-    // update the token's current owner
-    cryptoboy.currentOwner = msg.sender;
-    // update the how many times this token was transfered
-    cryptoboy.numberOfTransfers += 1;
-    // set and update that token in the mapping
-    allCryptoBoys[_tokenId] = cryptoboy;
-  }*/
-
+  
   function changeTokenPrice(uint256 _tokenId, uint256 _newPrice) public {
-    // require caller of the function is not an empty address
     require(msg.sender != address(0));
-    // require that token should exist
     require(_exists(_tokenId));
-    // get the token's owner
     address tokenOwner = ownerOf(_tokenId);
-    // check that token's owner should be equal to the caller of the function
     require(tokenOwner == msg.sender);
-    // get that token from all crypto boys mapping and create a memory of it defined as (struct => CryptoBoy)
     CryptoBoy memory cryptoboy = allCryptoBoys[_tokenId];
-    // update token's price with new price
     cryptoboy.price = _newPrice;
-    // set and update that token in the mapping
     allCryptoBoys[_tokenId] = cryptoboy;
   }
 
-  // switch between set for sale and set not for sale
- /* function toggleForSale(uint256 _tokenId) public {
-    // require caller of the function is not an empty address
-    require(msg.sender != address(0));
-    // require that token should exist
-    require(_exists(_tokenId));
-    // get the token's owner
-    address tokenOwner = ownerOf(_tokenId);
-    // check that token's owner should be equal to the caller of the function
-    require(tokenOwner == msg.sender);
-    // get that token from all crypto boys mapping and create a memory of it defined as (struct => CryptoBoy)
-    CryptoBoy memory cryptoboy = allCryptoBoys[_tokenId];
-    // if token's forSale is false make it true and vice versa
-    if(cryptoboy.forSale) {
-      cryptoboy.forSale = false;
-    } else {
-      cryptoboy.forSale = true;
-      //cryptoboy.forBidding = false;
-    }
-    // set and update that token in the mapping
-    allCryptoBoys[_tokenId] = cryptoboy;
-  }*/
  
   function toggleforBidding(uint256 _tokenId) public {
 
